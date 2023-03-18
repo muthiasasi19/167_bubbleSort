cb028ce1bb664d0dde5df956e4779ad539a05cb0
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-17 18:41:35 -0700

    Mengurutkan array menggunakan metode bubble sort

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -37,3 +37,15 @@ void display() {  // Procedure untuk menampilkan hasil
 	}
 	cout << endl;                               // Output basir kosong
 } 
+
+void bubbleSortArray() {     // Procedure untuk mengurutkan array dengan metode bubble Sort
+	for (int i = 1; < n; i++) {      //Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - i; j++) {        // Looping dengan j dimulai dari n-i-1 
+			if (a[j] > a[j + 1]) {          // Jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];            // Simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];            // Assign nilai a[j+1]
+				a[j + 1] = temp;            // Assign nilai temp ke a[j+1]
+			}
+		}
+	}
+}
